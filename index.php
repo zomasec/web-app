2b9f9d628e2c56fbb7cbd7afc2210275e720c443
Hazem El-Sayed <118324667+zoma0x00@users.noreply.github.com>
2023-08-01 04:37:28 +0300

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
-<html>
-<body>
+<?php 
 <?php include 'include/header.php' ?>
 <?php include 'include/nav.php' ?>
 <?php include 'include/footer.php' ?>
 
 <h1 align = 'center' >Home Page</h1> 
  
-</body>
-</html>
+?>
