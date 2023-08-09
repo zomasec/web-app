31afe9726965501b2d525102e9e40fccf2240837
Hazem El-Sayed <118324667+zoma0x00@users.noreply.github.com>
2023-08-09 21:21:42 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
-<?php 
-<?php include 'include/header.php' ?>
-<?php include 'include/nav.php' ?>
-<?php include 'include/footer.php' ?>
+<?php include 'include/header.php'; ?>
+<?php include 'include/nav.php'; ?>
+<?php include 'include/footer.php'; ?>
 
-<h1 align = 'center' >Home Page</h1> 
- 
-?>
+<h1 align='center'>Home Page</h1>
+
